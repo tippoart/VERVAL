a30ca3a5dcb3f0ee502a5ac9e3ecc2c4e69cc5e9
tippoart <latifardiansyah982@gmail.com>
2024-06-23 10:33:19 +0700

    update baru nih

--- home.js
+++ home.js
@@ -81,11 +81,11 @@ window.addEventListener("wheel", function (e) {
 });
 
 // Memeriksa kapan bagian (`section`) berada dalam tampilan untuk membuat kartu tetap (`fixed`)
+
 const section = document.getElementById("section2");
 
 window.addEventListener("scroll", function () {
   const sectionRect = section.getBoundingClientRect();
-
 
   if (sectionRect.top <= 0 && sectionRect.bottom >= window.innerHeight) {
     imageContainer.classList.add("fixed");
@@ -94,9 +94,181 @@ window.addEventListener("scroll", function () {
   }
 });
 
+//
+
+document.addEventListener("DOMContentLoaded", function () {
+  const textElement = document.querySelector(".emb");
+  const textElement2 = document.querySelector(".emb2");
+  const bumiIcon = document.querySelector(".bumi");
+  const iconContainer = document.querySelector(".icon");
+  const ourValuesSpan = document.querySelector(".our-values");
+  const future = document.querySelector(".future");
+
+  const cardContainers = [
+    document.querySelector(".verval3-card-img-1"),
+    document.querySelector(".verval3-card-img-2"),
+    document.querySelector(".verval3-card-img-3"),
+    document.querySelector(".verval3-card-img-4"),
+    document.querySelector(".verval3-card-img-5"),
+  ];
+
+  const spans = textElement.querySelectorAll("span");
+  let scrollStage = 0;
+
+  window.addEventListener("scroll", function () {
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+    const sectionOffsetTop = document.querySelector("#section3").offsetTop;
+    const sectionHeight = document.querySelector("#section3").offsetHeight;
+
+    // Menghitung posisi awal dan akhir dari section
+    const sectionStart = sectionOffsetTop;
+    const sectionEnd = sectionOffsetTop + sectionHeight;
+
+    if (scrollPosition >= sectionStart && scrollPosition <= sectionEnd) {
+      if (scrollStage === 0) {
+        // Animasi perubahan warna teks saat scroll pertama kali
+        textElement2.style.color = "white";
+        textElement.style.color = "white";
+        bumiIcon.style.transition = "0.5s";
+        bumiIcon.style.transform = "translateX(4.8rem)";
+        bumiIcon.style.backgroundColor = "#3ABEF9";
+        bumiIcon.style.borderRadius = "3.5rem";
+        iconContainer.style.backgroundColor = "white";
+        spans.forEach((span) => {
+          span.style.color = "white";
+        });
+
+        scrollStage = 1; // Update tahap scroll
+      } else if (
+        scrollStage === 1 &&
+        scrollPosition > sectionStart + sectionHeight / 10
+      ) {
+        // Jalankan fungsi scroll kedua kali
+        ourValuesSpan.style.color = "green";
+        ourValuesSpan.style.transition = "color 0.5s";
+        future.style.color = "white";
+        future.style.transition = "color 0.5s";
+
+        scrollStage = 2; // Update tahap scroll
+      } else if (
+        scrollStage === 2 &&
+        scrollPosition > sectionStart + sectionHeight / 5
+      ) {
+        // Jalankan fungsi scroll ketiga kali
+        textElement.style.opacity = "0";
+        textElement.style.transition = "1s";
+        iconContainer.style.opacity = "0";
+        iconContainer.style.transition = "1s";
+        textElement2.style.opacity = "0";
+        textElement2.style.transition = "1s";
+        bumiIcon.style.opacity = "0";
+        bumiIcon.style.transition = "1s";
+        ourValuesSpan.style.opacity = "0";
+        ourValuesSpan.style.transition = "1s";
+        future.style.opacity = "0";
+        future.style.transition = "1s";
+
+        cardContainers[0].style.display = "block";
+        
+        scrollStage = 3; // Update tahap scroll
+      } else if (
+        scrollStage === 3 &&
+        scrollPosition > sectionStart + sectionHeight / 4
+      ) {
+        // Jalankan fungsi scroll keempat kali
+        cardContainers[1].style.display = "block";
+       cardContainers[0].style.transform = ` scale(0.93)`;
+       
+       scrollStage = 4; // Update tahap scroll
+      } else if (
+        scrollStage === 4 &&
+        scrollPosition > sectionStart + sectionHeight / 3
+      ) {
+        // Jalankan fungsi scroll kelima kali
+        cardContainers[2].style.display = "block";
+        cardContainers[1].style.transform = ` scale(0.94)`;
+        cardContainers[0].style.transform = ` scale(0.85)`;
+        
+        scrollStage = 5; // Update tahap scroll
+      } else if (
+        scrollStage === 5 &&
+        scrollPosition > sectionStart + sectionHeight / 2.5
+      ) {
+        // Jalankan fungsi scroll keenam kali
+        cardContainers[3].style.display = "block";
+        // Turunkan card ketiga
+        cardContainers[1].style.transform = ` scale(0.90)`;
+        cardContainers[2].style.transform = ` scale(0.96)`;
+        cardContainers[0].style.transform = `translateY(85px) scale(0.91)`;
+        
+        scrollStage = 6; // Update tahap scroll
+      } else if (
+        scrollStage === 6 &&
+        scrollPosition > sectionStart + sectionHeight / 2
+      ) {
+        // Jalankan fungsi scroll ketujuh kali
+        cardContainers[4].style.display = "block";
+        
+        cardContainers[2].style.transform = ` scale(0.90)`;
+        cardContainers[3].style.transform = ` scale(0.94)`;
+        // Turunkan card keempat
+        cardContainers[1].style.transform = `translateY(50px) scale(0.91)`;
+
+        scrollStage = 7; // Update tahap scroll
+      }
+    } else {
+      // Reset warna huruf jika scroll berada di luar area yang ditentukan
+      spans.forEach((span) => {
+        span.style.color = "rgba(43, 47, 49, 0.915)";
+      });
+      textElement2.style.color = "rgba(43, 47, 49, 0.915)";
+      textElement2.style.opacity = "1";
+      textElement.style.color = "rgba(43, 47, 49, 0.915)";
+      textElement.style.opacity = "1";
+      ourValuesSpan.style.color = "rgba(43, 47, 49, 0.915)";
+      ourValuesSpan.style.opacity = "1";
+      future.style.color = "rgba(43, 47, 49, 0.915)";
+      future.style.opacity = "1";
+      iconContainer.style.backgroundColor = "transparent";
+      iconContainer.style.opacity = "1";
+      bumiIcon.style.transition = "0.5s";
+      bumiIcon.style.transform = "translateX(0)";
+      bumiIcon.style.backgroundColor = "transparent";
+      bumiIcon.style.borderRadius = "3rem";
+      bumiIcon.style.opacity = "1";
+
+      textElement.style.display = "block";
+      textElement2.style.display = "block";
+      bumiIcon.style.display = "block";
+      ourValuesSpan.style.display = "block";
+      future.style.display = "block";
+      cardContainers.forEach((card) => {
+        card.style.display = "none";
+        card.style.transform = "none"; // Reset posisi dan scale card
+      });
+
+      scrollStage = 0; // Reset tahap scroll
+    }
+  });
+
+  // Set warna huruf awalnya menjadi hitam saat halaman dimuat
+  spans.forEach((span) => {
+    span.style.color = "rgba(43, 47, 49, 0.915)";
+  });
+  textElement2.style.color = "rgba(43, 47, 49, 0.915)";
+  textElement.style.color = "rgba(43, 47, 49, 0.915)";
+  ourValuesSpan.style.color = "rgba(43, 47, 49, 0.915)";
+  future.style.color = "rgba(43, 47, 49, 0.915)";
+});
+
+
+
+
 
 
 // verval3
+
 // let clickCount = 0;
 // let emb = document.getElementsByClassName("emb");
 // let emb2 = document.getElementsByClassName("emb2");
@@ -184,80 +356,19 @@ window.addEventListener("scroll", function () {
 // }
 
 
-document.addEventListener("DOMContentLoaded", function () {
-  const textElement = document.querySelector(".emb");
 
-  // Mengambil teks dari elemen p yang sesuai
-  const textContent = textElement.textContent.trim();
 
-  // Hanya memecah teks menjadi span per huruf jika teks cocok dengan "emb dying the essence of better future, these are our values"
-  if (
-    textContent.includes(
-      "emb dying the essence of better future, these are our values"
-    )
-  ) {
-    textElement.innerHTML = "";
 
-    for (let i = 0; i < textContent.length; i++) {
-      const span = document.createElement("span");
-      span.textContent = textContent[i];
-      textElement.appendChild(span);
-    }
 
-    const allSpans = document.querySelectorAll(".emb span");
 
-    window.addEventListener("scroll", function (e) {
-      const sectionRect = document
-        .getElementById("section3")
-        .getBoundingClientRect();
-      const scrollPosition = window.scrollY;
-      const windowHeight = window.innerHeight;
 
-      // Menentukan apakah section berada dalam tampilan
-      if (
-        sectionRect.top <= windowHeight * 0.1 &&
-        sectionRect.bottom >= windowHeight * 0.1
-      ) {
-        textElement.classList.add("fixed-text");
-      } else {
-        textElement.classList.remove("fixed-text");
-      }
 
-      // Menghitung persentase scroll dari keseluruhan tinggi dokumen
-      const documentHeight = document.documentElement.scrollHeight;
-      const scrollPercentage = scrollPosition / (documentHeight - windowHeight);
 
-      // Menghitung jumlah huruf yang harus diwarnai, dimulai dari huruf "e"
-      const totalLetters = allSpans.length;
-      const lettersToColor = Math.max(
-        0,
-        Math.floor(
-          scrollPercentage * totalLetters - (textContent.indexOf("e") + 1)
-        )
-      );
 
-      // Mewarnai huruf-huruf sesuai dengan posisi scroll
-      allSpans.forEach((span, index) => {
-        if (
-          index >= textContent.indexOf("e") &&
-          index < textContent.indexOf("e") + lettersToColor
-        ) {
-          span.style.color = "white";
-        } else {
-          span.style.color = ""; // Kembalikan warna huruf menjadi default
-        }
-      });
 
-      // Mengembalikan warna huruf menjadi default saat scroll ke atas
-      const scrollDirection = Math.sign(e.deltaY);
-      if (scrollDirection === -1) {
-        allSpans.forEach((span) => {
-          span.style.color = ""; // Kembalikan warna huruf menjadi default
-        });
-      }
-    });
-  }
-});
+
+
+
 
 
 
