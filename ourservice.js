edfd12516428baeb89b657956913893700dcd011
tippoart <latifardiansyah982@gmail.com>
2024-06-27 19:20:24 +0700

    first commit

---
+++ ourservice.js
@@ -0,0 +1,11 @@
+function gambar() {
+  const gambar1 = document.querySelector(".img1");
+  const gambar2 = document.querySelector(".img2");
+
+  if (gambar1 && gambar2) {
+    gambar1.style.transform = "translateX(-100%)";
+    gambar2.style.transform = "translateX(-47%)";
+
+    
+  }
+}
