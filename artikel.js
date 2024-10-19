08f44836dd07d142d880d5d6f16e76c9b3bceff6
tippoart <latifardiansyah982@gmail.com>
2024-10-19 16:19:17 +0700

    first commit

--- artikel.js
+++ artikel.js
@@ -1,14 +1,35 @@
 const KlikMenu = document.getElementById("klik-menu");
 
 KlikMenu.addEventListener("click", function () {
-  const liElements = document.querySelectorAll("ul.nav-ul-head li");
+  const liElements = document.querySelectorAll("ul.navbar-nav li");
   const ContackUs = document.querySelector(".contact-us");
   const ul = document.querySelector("ul");
+  const LogoBrand = document.querySelector(".logo-brand");
+
+  const icon1 = document.querySelector(".bx-menu");
+  const icon2 = document.querySelector(".bx-x");
+
+  // Toggle icon display
+  if (icon1.style.display === "none") {
+    icon1.style.display = "block"; // Show menu icon
+    icon2.style.display = "none"; // Hide close icon
+  } else {
+    icon1.style.display = "none"; // Hide menu icon
+    icon2.style.display = "block"; // Show close icon
+  }
 
   if (ContackUs.style.display === "block") {
     ContackUs.style.display = "none";
   } else {
     ContackUs.style.display = "block";
+  }
+
+  // Toggle LogoBrand display
+  if (LogoBrand.style.display === "block") {
+    LogoBrand.style.display = "none";
+  } else {
+    LogoBrand.style.display = "block";
+    LogoBrand.style.zIndex = "4";
   }
 
   liElements.forEach(function (li) {
--- nav.js
+++ nav.js
@@ -3,15 +3,29 @@ const KlikMenu = document.getElementById("klik-menu");
 KlikMenu.addEventListener("click", function () {
   const liElements = document.querySelectorAll("ul.navbar-nav li");
   const ContackUs = document.querySelector(".contact-us");
-  const LogoBrand= document.querySelector(".logo-brand");
+  const LogoBrand = document.querySelector(".logo-brand");
   const ul = document.querySelector("ul");
 
+  const icon1 = document.querySelector(".bx-menu");
+  const icon2 = document.querySelector(".bx-x");
+
+  // Toggle icon display
+  if (icon1.style.display === "none") {
+    icon1.style.display = "block"; // Show menu icon
+    icon2.style.display = "none"; // Hide close icon
+  } else {
+    icon1.style.display = "none"; // Hide menu icon
+    icon2.style.display = "block"; // Show close icon
+  }
+
+  // Toggle ContactUs display
   if (ContackUs.style.display === "block") {
     ContackUs.style.display = "none";
   } else {
     ContackUs.style.display = "block";
-   
   }
+
+  // Toggle LogoBrand display
   if (LogoBrand.style.display === "block") {
     LogoBrand.style.display = "none";
   } else {
@@ -19,6 +33,7 @@ KlikMenu.addEventListener("click", function () {
     LogoBrand.style.zIndex = "4";
   }
 
+  // Toggle li elements display
   liElements.forEach(function (li) {
     if (li.style.display === "block") {
       ul.style.backgroundColor = "transparent";
