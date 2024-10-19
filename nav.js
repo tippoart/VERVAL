7d9b8ef773d46a8f1d0ec263a4d1bc97e273b2eb
tippoart <latifardiansyah982@gmail.com>
2024-10-19 15:24:02 +0700

    first commit

--- nav.js
+++ nav.js
@@ -3,12 +3,20 @@ const KlikMenu = document.getElementById("klik-menu");
 KlikMenu.addEventListener("click", function () {
   const liElements = document.querySelectorAll("ul.navbar-nav li");
   const ContackUs = document.querySelector(".contact-us");
+  const LogoBrand= document.querySelector(".logo-brand");
   const ul = document.querySelector("ul");
 
   if (ContackUs.style.display === "block") {
     ContackUs.style.display = "none";
   } else {
     ContackUs.style.display = "block";
+   
+  }
+  if (LogoBrand.style.display === "block") {
+    LogoBrand.style.display = "none";
+  } else {
+    LogoBrand.style.display = "block";
+    LogoBrand.style.zIndex = "4";
   }
 
   liElements.forEach(function (li) {
